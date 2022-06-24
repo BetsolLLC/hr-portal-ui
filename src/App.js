bca6f37faa7b0776401cf9ac86a285f428ee1dc5
Prateek <Prateek.Kunjal@betsol.com>
2022-06-24 12:26:15 -0700

    add admin route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Onboarding from "./screens/Onboarding/Onboarding";
+import Admin from "./screens/Admin/Admin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Onboarding />} />
+        {/* <Route path="/" element={<Onboarding />} /> */}
+        <Route path="/" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
