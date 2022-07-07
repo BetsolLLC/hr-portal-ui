22a40ae6fe43bc1a25def7488b0fae374397f7fb
Prateek <Prateek.Kunjal@betsol.com>
2022-07-07 05:19:25 -0700

    swap empty set and null parameters

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -3,13 +3,13 @@ import { createContext, useEffect, useState } from "react";
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     setUser(
       localStorage.getItem("userInfo")
         ? JSON.parse(localStorage.getItem("userInfo"))
-        : null
+        : {}
     );
   }, []);
   const setUserInfo = (userInfo) => {
