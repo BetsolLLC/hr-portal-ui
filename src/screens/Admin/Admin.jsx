d28095251ad66c1133ab5b5075d167e80324966d
Prateek <Prateek.Kunjal@betsol.com>
2022-07-07 05:20:04 -0700

    implement add user api

--- src/screens/Admin/Admin.jsx
+++ src/screens/Admin/Admin.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 
 import {
   FormControl,
@@ -15,107 +14,140 @@ import {
   ModalBody,
   ModalCloseButton,
   Flex,
-    Stack,
-    Spacer,
-    VStack,
-    Box,
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-    Text,
-    Button,
-    ButtonGroup,
-    useDisclosure
-  } from '@chakra-ui/react'
+  Stack,
+  Spacer,
+  VStack,
+  Box,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Text,
+  Button,
+  ButtonGroup,
+  useDisclosure,
+  Spinner,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+} from "@chakra-ui/react";
+import { AddUserModal } from "./components/AddUserModal";
+import axios from "../../api/axios";
+import Header from "../../components/header";
 
-  const Admin = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    return(
-        <Box p={32}  bg="blue.50" height="100vh">
-            <Flex spacing={4} mb={12} direction='row' alignItems='center'>
-              <Box width='3xl'><Text color="teal" fontSize='xl'>Hello <Text fontWeight='bold' fontSize='3xl'>Admin</Text></Text></Box>
-              <Spacer></Spacer>
-            <Box>
-            <Button colorScheme='blue' variant='ghost' onClick={onOpen}>Add user</Button>
-            <Button colorScheme='blue' onClick={onOpen} >Add multiple users</Button>
-            </Box>
-            
-            
-            </Flex>
-           
-            <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add new user</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-          <FormControl>
-  <FormLabel htmlFor='email'>Email address</FormLabel>
-  <Input id='email' type='email' />
- 
-</FormControl>
-          </ModalBody>
+const Admin = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-          <ModalFooter>
-            <Button colorScheme='teal' mr={3} onClick={onClose}>
-              Submit
+  useEffect(() => {
+    setLoading(true);
+    //  add api in the url
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const config = {
+          headers: {
+            // Authorization: `Bearer ${user.jwtToken}`,
+            "Content-Type": "application/json",
+          },
+        };
+        const {
+          data: { success, error, data },
+        } = await axios.get("/api/auth/login", config); //api goes here
+        if (success) {
+          setData(data);
+        } else {
+          setError(error);
+        }
+        setLoading(false);
+      } catch (err) {
+        setError(
+          err?.response?.data?.error
+            ? err.response.data.error
+            : "Something went wrong.Please try again."
+        );
+
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Box p={32} bg="blue.50" height="100vh">
+        <Flex spacing={4} mb={12} direction="row" alignItems="center">
+          <Box width="3xl">
+            <Text color="teal" fontSize="xl">
+              Hello{" "}
+              <Text fontWeight="bold" fontSize="3xl">
+                Admin
+              </Text>
+            </Text>
+          </Box>
+          <Spacer></Spacer>
+          <Box>
+            <Button colorScheme="blue" variant="ghost" onClick={onOpen}>
+              Add user
             </Button>
-           
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    
-        <Box maxW='6xl' boxShadow="lg" bg="white" borderRadius="xl">
-           
-        <TableContainer p={10}>
-        <Table variant='simple' colorScheme='gray'>
-          
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Mail</Th>
-              <Th isNumeric>Phone No.</Th>
-              <Th isNumeric>Docs Uploaded</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>Nagamma</Td>
-              <Td> nagu@gmail.com</Td>
-              <Td isNumeric>9999999999</Td>
-              <Td isNumeric>5</Td>
-            </Tr>
-            <Tr>
-              <Td>Dinesh</Td>
-              <Td> dinesh@gmail.com</Td>
-              <Td isNumeric>9999999999</Td>
-              <Td isNumeric>2</Td>
-            </Tr>
-            <Tr>
-              <Td>Lakshmamma</Td>
-              <Td> luckyamma@gmail.com</Td>
-              <Td isNumeric>9999999999</Td>
-              <Td isNumeric>7</Td>
-            </Tr>
-            <Tr>
-              <Td>Ansan</Td>
-              <Td> pigeonnest@gmail.com</Td>
-              <Td isNumeric>8888888888</Td>
-              <Td isNumeric>3</Td>
-            </Tr>
-          </Tbody>
-         
-        </Table>
-      </TableContainer>
+            {/* <Button colorScheme="blue" onClick={onOpen}>
+            Add multiple users
+          </Button> */}
+          </Box>
+        </Flex>
+
+        <AddUserModal isOpen={isOpen} onClose={onClose} />
+        {loading ? (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="blue.500"
+            size="xl"
+          />
+        ) : error ? (
+          <Alert status="error">
+            <AlertIcon />
+            <AlertDescription>{error}</AlertDescription>
+          </Alert>
+        ) : (
+          <>
+            <Box maxW="6xl" boxShadow="lg" bg="white" borderRadius="xl">
+              <TableContainer p={10}>
+                <Table variant="simple" colorScheme="gray">
+                  <Thead>
+                    <Tr>
+                      <Th>Name</Th>
+                      <Th>Mail</Th>
+                      <Th isNumeric>Phone No.</Th>
+                      <Th isNumeric>Docs Uploaded</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {data?.map((doc) => (
+                      <Tr>
+                        <Td>{doc.name}</Td>
+                        <Td> {doc.mail}</Td>
+                        <Td isNumeric>{doc.phNumber}</Td>
+                        <Td isNumeric>{doc.no}</Td>
+                      </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Box>
+          </>
+        )}
       </Box>
-      </Box>
-    )
-}
+    </>
+  );
+};
 
 export default Admin;
---
+++ src/screens/Admin/components/AddUserModal.jsx
@@ -0,0 +1,150 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+  VStack,
+  Text,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import React, { useContext, useState } from "react";
+import axios from "../../../api/axios";
+import AuthContext from "../../../context/AuthProvider";
+
+export const AddUserModal = ({ isOpen, onClose }) => {
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const { user } = useContext(AuthContext);
+
+  const handleEmail = async (name, email, batch, number) => {
+    setLoading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          access_token: user.jwtToken,
+        },
+      };
+      const {
+        data: { success, error, data },
+      } = await axios.post(
+        "/api/auth/addusers",
+        { name, email, batch, number },
+        config
+      );
+      if (success) {
+        toast({
+          title: "Success",
+          description: "User added succesfully",
+          status: "success",
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: error,
+          status: "error",
+        });
+      }
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error",
+        description: error.response.data.error
+          ? error.response.data.error
+          : "Something went wrong.Please try again.",
+        status: "error",
+      });
+      setLoading(false);
+    }
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      batch: "",
+      number: "",
+    },
+    onSubmit: (values) => {
+      handleEmail(values.name, values.email, values.batch, values.number);
+    },
+  });
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <form onSubmit={formik.handleSubmit}>
+        <ModalContent>
+          <ModalHeader>Add new user</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={8} align="flex-start">
+              <Text align={"center"} fontWeight="bold" fontSize="3xl">
+                Enter Details
+              </Text>
+
+              <FormControl isRequired>
+                <FormLabel>Name</FormLabel>
+                <Input
+                  id="name"
+                  name="name"
+                  type="name"
+                  variant="filled"
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Mail</FormLabel>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  variant="filled"
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Batch</FormLabel>
+                <Input
+                  id="batch"
+                  name="batch"
+                  type="batch"
+                  variant="filled"
+                  onChange={formik.handleChange}
+                  value={formik.values.batch}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Number</FormLabel>
+                <Input
+                  id="number"
+                  name="number"
+                  type="number"
+                  variant="filled"
+                  onChange={formik.handleChange}
+                  value={formik.values.number}
+                />
+              </FormControl>
+            </VStack>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button isLoading={loading} colorScheme="teal" mr={3} type="submit">
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
+};
